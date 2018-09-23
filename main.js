11d568808122d39462a1d94e5dab18bf649e4f87
Kegen Guyll <guyllkegen@gmail.com>
2018-09-22 21:06:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function section2() {
+    document.getElementById('section2').scrollIntoView({block: 'start', behavior: 'smooth'});
+  }
+
+function section1() {
+    document.getElementById('section1').scrollIntoView({block: 'start', behavior: 'smooth'});
+  }
+
+function section3() {
+    document.getElementById('section3').scrollIntoView({block: 'start', behavior: 'smooth'});
+  }
+
+  function home() {
+    document.getElementById('home').scrollIntoView({block: 'start', behavior: 'smooth'});
+  }
